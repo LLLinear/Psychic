5de6f8dcd5e48b88fc9950d8c41ed5cbd8dd0d2a
LLLinear <chlinear@gmail.com>
2023-08-10 11:41:36 +0900

    🎈 플러그인 기본 파일 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,19 @@
 plugins {
     kotlin("jvm") version "1.8.0"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
-group = "com.github.lllinear.psychic"
+group = "com.github.lllinear"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    mavenLocal()
 }
 
 dependencies {
     testImplementation(kotlin("test"))
+    compileOnly("org.spigotmc:spigot:1.19.4-R0.1-SNAPSHOT")
 }
 
 tasks.test {
---
+++ src/main/kotlin/com/github/lllinear/psychic/Psychic.kt
@@ -0,0 +1,6 @@
+package com.github.lllinear.psychic
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Psychic: JavaPlugin() {
+}
