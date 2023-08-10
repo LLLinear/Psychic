e01ab277e290a9b8b754867fbab2f77c6e67ab18
LLLinear <chlinear@gmail.com>
2023-08-10 12:00:49 +0900

    🎈 onEnable 추가

--- src/main/kotlin/com/github/lllinear/psychics/Psychics.kt
+++ src/main/kotlin/com/github/lllinear/psychics/Psychics.kt
@@ -1,6 +1,30 @@
 package com.github.lllinear.psychics
 
+import org.bukkit.Bukkit
+import org.bukkit.ChatColor
+import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
+import kotlin.math.log
 
 class Psychics: JavaPlugin() {
+    companion object {
+        val prefix = "&l[ %dPsychics &f] "
+
+        fun sendMessage(player: Player, message: String): Boolean {
+            if (player == null) {
+                return false
+            }
+
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + message))
+            return true
+        }
+
+        fun broadcastMessage(message: String) {
+            Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', prefix + message))
+        }
+    }
+
+    override fun onEnable() {
+        logger.info("Plugin Enabled.")
+    }
 }
